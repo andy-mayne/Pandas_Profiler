24c7f2be5dc54370a4c81ac441ca3bcd23a00ebd
Andy <98969758+andy-mayne@users.noreply.github.com>
2022-02-09 18:07:23 +0000

    Update profile.py

--- pandasprofiling/profile.py
+++ pandasprofiling/profile.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas_profiling
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import pandas as pd
 import PIL
